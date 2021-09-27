d708a697553a311b5bf7c234b2a454cca9aebf41
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-09-27 15:53:56 +0100

    [PRMT-2283] Added Java skeleton src and build

---
+++ src/main/java/uk/nhs/prm/deductions/nemseventprocessor/NemsEventProcessorApplication.java
@@ -0,0 +1,13 @@
+package uk.nhs.prm.deductions.nemseventprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemsEventProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NemsEventProcessorApplication.class, args);
+    }
+
+}
