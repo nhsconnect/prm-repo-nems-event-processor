d573d04efe481f6efa22341fba2977305c24174a
Dan Moore <dan.moore1@nhs.net>
2021-09-29 18:17:49 +0100

    [PRMT-2283] Checking recent metric data has been sent.
    Not yet polling, may fail on data not yet consistent.
    
    Co-authored-by: tomzo

--- src/integrationtest/java/uk/nhs/prm/deductions/nemseventprocessor/metrics/MetricsClientTest.java
+++ src/integrationtest/java/uk/nhs/prm/deductions/nemseventprocessor/metrics/MetricsClientTest.java
@@ -1,17 +1,23 @@
 package uk.nhs.prm.deductions.nemseventprocessor.metrics;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
-import software.amazon.awssdk.services.cloudwatch.model.Dimension;
-import software.amazon.awssdk.services.cloudwatch.model.ListMetricsRequest;
-import software.amazon.awssdk.services.cloudwatch.model.ListMetricsResponse;
-import software.amazon.awssdk.services.cloudwatch.model.Metric;
+import software.amazon.awssdk.services.cloudwatch.model.*;
 
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
 
+import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class MetricsClientTest {
@@ -23,24 +29,70 @@ class MetricsClientTest {
         HealthMetricPublisher publisher = new HealthMetricPublisher(cloudWatchClient);
 
         publisher.publishHealthyStatus();
-        
+
         List<Metric> metrics = fetchMetricsMatching("PrmDeductions/NemsEventProcessor", "Health");
         assertThat(metrics).isNotEmpty();
-        assertThat(filterForMetricsMatchingDimension(metrics, "Environment", "ci")).isNotEmpty();
+
+        MetricDataResult metricData = fetchRecentMetricData(2, getMetricWhere(metrics, metricHasDimension("Environment", "ci")));
+
+        System.out.println(metricData.values());
+        System.out.println(metricData.timestamps());
+
+        assertThat(metricData.values()).isNotEmpty();
+        assertThat(metricData.values().get(0)).isEqualTo(1.0);
     }
 
-    private List<Metric> filterForMetricsMatchingDimension(List<Metric> metrics, String dimensionName, String dimensionValue) {
-        return metrics.stream().filter(metric -> metric.dimensions().stream().anyMatch(dimension ->
-                dimension.name().equals(dimensionName) && dimension.value().equals(dimensionValue))).collect(Collectors.toList());
+    @NotNull
+    private Predicate<Metric> metricHasDimension(String name, String value) {
+        return metric -> metric.dimensions().stream().anyMatch(dimension ->
+                dimension.name().equals(name) && dimension.value().equals(value));
     }
 
+    private <Metric> Predicate<Metric> all(Predicate<Metric>... predicates) {
+        return Arrays.stream(predicates).reduce(Predicate::and).orElse(x -> true);
+    }
+
+    private Metric getMetricWhere(List<Metric> metrics, Predicate<Metric> metricPredicate) {
+        List<Metric> filteredMetrics = metrics.stream().filter(metricPredicate).collect(toList());
+        assertThat(filteredMetrics.size()).isEqualTo(1);
+        return filteredMetrics.get(0);
+    }
     private List<Metric> fetchMetricsMatching(String namespace, String metricName) {
         ListMetricsRequest request = ListMetricsRequest.builder()
                 .namespace(namespace)
                 .metricName(metricName)
+                .recentlyActive(RecentlyActive.PT3_H)
                 .build();
 
         ListMetricsResponse listMetricsResponse = cloudWatchClient.listMetrics(request);
         return listMetricsResponse.metrics();
+    }
+
+    private MetricDataResult fetchRecentMetricData(int minutesOfRecency, Metric metric) {
+        MetricDataQuery dataQuery = MetricDataQuery.builder()
+                .id("health_test_query")
+                .metricStat(MetricStat.builder()
+                        .metric(metric)
+                        .period(1)
+                        .stat("Minimum")
+                        .build())
+                .returnData(true)
+                .build();
+        GetMetricDataRequest request = GetMetricDataRequest.builder()
+                .startTime(Instant.now().minusSeconds(minutesOfRecency * 60).truncatedTo(ChronoUnit.MINUTES))
+                .endTime(Instant.now().truncatedTo(ChronoUnit.MINUTES))
+                .metricDataQueries(dataQuery)
+                .build();
+
+        List<MetricDataResult> metricDataResults = cloudWatchClient.getMetricData(request).metricDataResults();
+        System.out.println("metric data results size: " + metricDataResults.size());
+        assertThat(metricDataResults.size()).isEqualTo(1);
+
+        MetricDataResult metricDataResult = metricDataResults.get(0);
+        assertThat(metricDataResult.statusCode()).isEqualTo(StatusCode.COMPLETE);
+        System.out.println("metric data result status: " + metricDataResult.statusCodeAsString());
+        System.out.println("metric data result hasValues: " + metricDataResult.hasValues());
+        System.out.println("metric data result hasTimestamps: " + metricDataResult.hasTimestamps());
+        return metricDataResult;
     }
 }
