a330c483af9b9e1708e44407bda56684355fadbf
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-10-01 14:51:09 +0100

    [PRMT-2283] Created health metric alarm

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -6,3 +6,13 @@ resource "aws_cloudwatch_log_group" "log_group" {
     CreatedBy= var.repo_name
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "health_metric_failure_alarm" {
+  alarm_name                = "health-metric-failure"
+  comparison_operator       = "LessThanThreshold"
+  threshold                 = "1"
+  evaluation_periods        = "3"
+  metric_name               = "Health"
+  namespace                 = "PrmDeductions/NemsEventProcessor"
+  alarm_description         = "Alarm to flag failed health checks"
+}
