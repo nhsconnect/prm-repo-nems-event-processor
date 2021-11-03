00976967a93455ea3ee4b3900ef356b4975b1292
Emily Youngs <emily.youngs1@nhs.net>
2021-11-03 12:13:05 +0000

    [PRMT-2359] Added parser for filtering by nhs number

--- src/main/java/uk/nhs/prm/deductions/nemseventprocessor/nemsevents/NemsEventParser.java
+++ src/main/java/uk/nhs/prm/deductions/nemseventprocessor/nemsevents/NemsEventParser.java
@@ -1,5 +1,6 @@
 package uk.nhs.prm.deductions.nemseventprocessor.nemsevents;
 
+import com.jcabi.xml.XML;
 import com.jcabi.xml.XMLDocument;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,7 @@ public class NemsEventParser {
             return NemsEventMessage.nonDeduction();
         }
 
-        String nhsNumber = new XMLDocument(messageBody).registerNs("fhir", "http://hl7.org/fhir")
-                .xpath("//fhir:Patient/fhir:identifier/fhir:value/@value").get(0);
+        String nhsNumber = messageXml.xpath("//fhir:Patient/fhir:identifier/fhir:value/@value").get(0);
 
         return NemsEventMessage.deduction(nhsNumber);
     }
--- src/test/java/uk/nhs/prm/deductions/nemseventprocessor/nemsevents/NemsEventParserTest.java
+++ src/test/java/uk/nhs/prm/deductions/nemseventprocessor/nemsevents/NemsEventParserTest.java
@@ -40,7 +40,7 @@ class NemsEventParserTest {
     }
 
     @Test
-    void shouldParseANemsMessageAsANonDeductionWhenGPFieldIsPresent(){
+    void shouldParseANemsMessageAsANonDeductionWhenGPFieldIsPresentInThePatientSection() {
         String messageBody= "<Bundle xmlns=\"http://hl7.org/fhir\">\n" +
                 "    <entry>\n" +
                 "        <resource>\n" +
@@ -80,5 +80,39 @@ class NemsEventParserTest {
         NemsEventMessage message = nemsEventParser.parse(messageBody);
 
         assertThat(message.getNhsNumber()).isEqualTo("9912003888");
+    }
+
+    @Test
+    void shouldParseANemsMessageAsADeductionWhenGPFieldIsPresentOnlyInNonPatientEntriesInTheMessage() {
+        String messageBody= "<Bundle xmlns=\"http://hl7.org/fhir\">\n" +
+                "    <entry>\n" +
+                "        <resource>\n" +
+                "            <Patient>\n" +
+                "                <meta>\n" +
+                "                    <profile value=\"https://fhir.hl7.org.uk/STU3/StructureDefinition/CareConnect-Patient-1\"/>\n" +
+                "                </meta>\n" +
+                "                <identifier>\n" +
+                "                    <system value=\"https://fhir.nhs.uk/Id/nhs-number\"/>\n" +
+                "                    <value value=\"9912003888\"/>\n" +
+                "                </identifier>\n" +
+                "            </Patient>\n" +
+                "        </resource>\n" +
+                "    </entry>\n" +
+                "    <entry>\n" +
+                "        <resource>\n" +
+                "            <Organization>\n" +
+                "                <generalPractitioner>\n" +
+                "                    <reference value=\"urn:uuid:59a63170-b769-44f7-acb1-1234abcd\"/>\n" +
+                "                    <display value=\"SHUDEHILL MEDICAL CENTRE\"/>\n" +
+                "                </generalPractitioner>\n" +
+                "            </Organization>\n" +
+                "        </resource>\n" +
+                "    </entry>\n" +
+                "</Bundle>";
+
+        NemsEventParser nemsEventParser = new NemsEventParser();
+        NemsEventMessage message = nemsEventParser.parse(messageBody);
+
+        assertTrue(message.isDeduction());
     }
 }
