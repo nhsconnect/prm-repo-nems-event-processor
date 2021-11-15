13aa3377dc524586f8be2298ea09448a1cbefc83
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-11-15 17:48:12 +0000

    [PRMT-2423] Renamed nems events queue to nems-events-processor-incoming-queue

--- terraform/queue.tf
+++ terraform/queue.tf
@@ -1,24 +1,28 @@
-resource "aws_sqs_queue" "nems_events" {
-  name                       = "${var.environment}-${var.component_name}-nems-events-queue"
+locals {
+  incoming_queue_name = "${var.environment}-${var.component_name}-incoming-queue"
+}
+
+resource "aws_sqs_queue" "incoming_nems_events" {
+  name                       = local.incoming_queue_name
   message_retention_seconds  = 1800
   kms_master_key_id = data.aws_ssm_parameter.sns_sqs_kms_key_id.value
 
   tags = {
-    Name = "${var.environment}-${var.component_name}-nems-events-queue"
+    Name = local.incoming_queue_name
     CreatedBy   = var.repo_name
     Environment = var.environment
   }
 }
 
-resource "aws_sns_topic_subscription" "nems_events_to_queue" {
+resource "aws_sns_topic_subscription" "nems_events_to_incoming_queue" {
   protocol             = "sqs"
   raw_message_delivery = true
   topic_arn            = data.aws_ssm_parameter.nems_events_topic_arn.value
-  endpoint             = aws_sqs_queue.nems_events.arn
+  endpoint             = aws_sqs_queue.incoming_nems_events.arn
 }
 
-resource "aws_sqs_queue_policy" "nems_events_subscription" {
-  queue_url = aws_sqs_queue.nems_events.id
+resource "aws_sqs_queue_policy" "incoming_nems_events_subscription" {
+  queue_url = aws_sqs_queue.incoming_nems_events.id
   policy    = data.aws_iam_policy_document.sqs_nems_event_policy_doc.json
 }
 
@@ -37,7 +41,7 @@ data "aws_iam_policy_document" "sqs_nems_event_policy_doc" {
     }
 
     resources = [
-      aws_sqs_queue.nems_events.arn
+      aws_sqs_queue.incoming_nems_events.arn
     ]
 
     condition {
