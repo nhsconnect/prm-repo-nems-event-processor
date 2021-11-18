f3a684be5933606516e6fb53ec2b1e2cd9784e68
Emily Youngs <emily.youngs1@nhs.net>
2021-11-18 11:46:23 +0000

    [PRMT-2423] Refactored HealthMetricPublisherTest with setUp method

--- src/test/java/uk/nhs/prm/deductions/nemseventprocessor/metrics/HealthMetricPublisherTest.java
+++ src/test/java/uk/nhs/prm/deductions/nemseventprocessor/metrics/HealthMetricPublisherTest.java
@@ -1,11 +1,13 @@
 package uk.nhs.prm.deductions.nemseventprocessor.metrics;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.nhs.prm.deductions.nemseventprocessor.metrics.healthprobes.HealthProbe;
 import uk.nhs.prm.deductions.nemseventprocessor.metrics.healthprobes.SqsHealthProbe;
+import uk.nhs.prm.deductions.nemseventprocessor.metrics.healthprobes.SuspensionsSnsHealthProbe;
 import uk.nhs.prm.deductions.nemseventprocessor.metrics.healthprobes.UnhandledSnsHealthProbe;
 
 import java.util.ArrayList;
@@ -15,38 +17,44 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class HealthMetricPublisherTest {
+private MetricPublisher metricPublisher;
+private SqsHealthProbe sqsHealthProbe;
+private UnhandledSnsHealthProbe unhandledSnsHealthProbe;
+private SuspensionsSnsHealthProbe suspensionsSnsHealthProbe;
+private List<HealthProbe> probe = new ArrayList<>();
+
+    @BeforeEach
+    void setUp(){
+        metricPublisher = Mockito.mock(MetricPublisher.class);
+        sqsHealthProbe = Mockito.mock(SqsHealthProbe.class);
+        unhandledSnsHealthProbe = Mockito.mock(UnhandledSnsHealthProbe.class);
+        suspensionsSnsHealthProbe = Mockito.mock(SuspensionsSnsHealthProbe.class);
+        probe.add(sqsHealthProbe);
+        probe.add(unhandledSnsHealthProbe);
+        probe.add(suspensionsSnsHealthProbe);
+    }
 
     @Test
     public void shouldSetHealthMetricToZeroForUnhealthyIfConnectionIsUnhealthy() {
-        MetricPublisher metricPublisher = Mockito.mock(MetricPublisher.class);
-        List<HealthProbe> probe = new ArrayList<>();
-        SqsHealthProbe sqsHealthProbe = Mockito.mock(SqsHealthProbe.class);
-        UnhandledSnsHealthProbe unhandledSnsHealthProbe = Mockito.mock(UnhandledSnsHealthProbe.class);
-        probe.add(sqsHealthProbe);
-        probe.add(unhandledSnsHealthProbe);
         when(sqsHealthProbe.isHealthy()).thenReturn(false);
         when(unhandledSnsHealthProbe.isHealthy()).thenReturn(false);
+        when(suspensionsSnsHealthProbe.isHealthy()).thenReturn(false);
 
         HealthMetricPublisher healthPublisher = new HealthMetricPublisher(metricPublisher,probe);
         healthPublisher.publishHealthStatus();
 
-        verify(metricPublisher,times(2)).publishMetric("Health", 0.0);
+        verify(metricPublisher,times(3)).publishMetric("Health", 0.0);
     }
 
     @Test
     public void shouldSetHealthMetricToOneIfConnectionIsHealthy() {
-        MetricPublisher metricPublisher = Mockito.mock(MetricPublisher.class);
-        List<HealthProbe> probe = new ArrayList<>();
-        SqsHealthProbe sqsHealthProbe = Mockito.mock(SqsHealthProbe.class);
-        UnhandledSnsHealthProbe unhandledSnsHealthProbe = Mockito.mock(UnhandledSnsHealthProbe.class);
-        probe.add(sqsHealthProbe);
-        probe.add(unhandledSnsHealthProbe);
         when(sqsHealthProbe.isHealthy()).thenReturn(true);
         when(unhandledSnsHealthProbe.isHealthy()).thenReturn(true);
+        when(suspensionsSnsHealthProbe.isHealthy()).thenReturn(true);
 
         HealthMetricPublisher healthPublisher = new HealthMetricPublisher(metricPublisher, probe);
         healthPublisher.publishHealthStatus();
-        verify(metricPublisher,times(2)).publishMetric("Health", 1.0);
+        verify(metricPublisher,times(3)).publishMetric("Health", 1.0);
     }
 
 }
