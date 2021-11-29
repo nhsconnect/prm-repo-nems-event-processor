1915ddd8130012f2e581ac68f033653423272cf9
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-11-29 10:43:20 +0000

    [PRMT-2313] Created alarms for suspensions and unhandled events sns topic errors

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,6 +1,10 @@
 locals {
   error_logs_metric_name                = "ErrorCountInLogs"
   nems_event_processor_metric_namespace = "NemsEventProcessor"
+  suspensions_sns_topic_name            = "${var.environment}-nems-event-processor-suspensions-sns-topic"
+  unhandled_events_sns_topic_name       = "${var.environment}-nems-event-processor-unhandled-events-sns-topic"
+  sns_topic_namespace = "AWS/SNS"
+  sns_topic_error_logs_metric_name = "NumberOfNotificationsFailed"
 }
 
 resource "aws_cloudwatch_log_group" "log_group" {
@@ -58,4 +62,40 @@ resource "aws_cloudwatch_metric_alarm" "error_log_alarm" {
 
 data "aws_sns_topic" "alarm_notifications" {
   name = "${var.environment}-alarm-notifications-sns-topic"
+}
+
+resource "aws_cloudwatch_metric_alarm" "suspensions_sns_topic_error_log_alarm" {
+  alarm_name                = "${local.suspensions_sns_topic_name}-error-logs"
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "0"
+  evaluation_periods        = "1"
+  period                    = "60"
+  metric_name               = local.sns_topic_error_logs_metric_name
+  namespace                 = local.sns_topic_namespace
+  dimensions = {
+    TopicName = local.suspensions_sns_topic_name
+  }
+  statistic                 = "Sum"
+  alarm_description         = "This alarm monitors errors logs in ${local.suspensions_sns_topic_name}"
+  treat_missing_data        = "notBreaching"
+  actions_enabled           = "true"
+  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "unhandled_events_sns_topic_error_log_alarm" {
+  alarm_name                = "${local.unhandled_events_sns_topic_name}-error-logs"
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "0"
+  evaluation_periods        = "1"
+  period                    = "60"
+  metric_name               = local.sns_topic_error_logs_metric_name
+  namespace                 = local.sns_topic_namespace
+  dimensions = {
+    TopicName = local.unhandled_events_sns_topic_name
+  }
+  statistic                 = "Sum"
+  alarm_description         = "This alarm monitors errors logs in ${local.unhandled_events_sns_topic_name}"
+  treat_missing_data        = "notBreaching"
+  actions_enabled           = "true"
+  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
 }
