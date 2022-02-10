7f7a37090e20448d8317c968d8a0edc361d6a886
Rosie Amphlett <rosie.amphlett1@nhs.net>
2022-02-10 14:08:15 +0000

    [PRMT-2502] Specified retrieval of messages with nemsMessageId attribute for integration test

--- src/integration/java/uk/nhs/prm/deductions/nemseventprocessor/nemsevents/NemsEventsIntegrationTest.java
+++ src/integration/java/uk/nhs/prm/deductions/nemseventprocessor/nemsevents/NemsEventsIntegrationTest.java
@@ -157,7 +157,7 @@ class NemsEventsIntegrationTest {
 
         await().atMost(5, TimeUnit.SECONDS).untilAsserted(() -> {
             System.out.println("checking sqs queue: " + receiving);
-            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest().withQueueUrl(receiving).withMessageAttributeNames("traceId");
+            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest().withQueueUrl(receiving).withMessageAttributeNames("traceId").withMessageAttributeNames("nemsMessageId");
             List<Message> messages = amazonSQSAsync.receiveMessage(receiveMessageRequest).getMessages();
             System.out.println("messages: " + messages.size());
             assertThat(messages).hasSize(1);
