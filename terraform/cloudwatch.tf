90052539759f4c85e0aa87f0f1ae026858165674
Mario Giampietri <mario.giampietri1@nhs.net>
2022-04-01 11:39:07 +0100

    [PRMT-2525] adjusted condition for triggering alarm correctly outside of working hours

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -234,10 +234,10 @@ resource "aws_cloudwatch_metric_alarm" "nems_unhandled_audit" {
 
 resource "aws_cloudwatch_metric_alarm" "nems_incoming_receiving_in_working_hours" {
   alarm_name                = "${var.environment}-nems-incoming-receiving-in-working-hours"
-  comparison_operator       = "LessThanOrEqualToThreshold"
+  comparison_operator       = "GreaterThanThreshold"
   evaluation_periods        = "320" # 4 hours (given period is set to 1 min/60sec)
   threshold                 = "0"
-  alarm_description         = "Alarm for when nems incoming messages are not coming over working hour"
+  alarm_description         = "Alarm for when nems incoming messages are not coming over working hour. In the graph 1 means alarm, 0 means no alarm"
   actions_enabled           = true
 
   metric_query {
