1814a0801ab92d2ae9108dffc72e21fde0463567
Mario Giampietri <mario.giampietri1@nhs.net>
2022-06-09 12:08:57 +0100

    [PRMT-CODETIME] removed ratio alarm

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -103,55 +103,6 @@ resource "aws_cloudwatch_metric_alarm" "unhandled_events_sns_topic_error_log_ala
   alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
 }
 
-
-resource "aws_cloudwatch_metric_alarm" "nems_incoming_queue_ratio_of_received_to_acknowledgement" {
-  alarm_name                = "${var.environment}-nems-incoming-queue-ratio-of-received-to-acknowledgement"
-  comparison_operator       = "LessThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  threshold                 = "50"
-  alarm_description         = "Received message ratio to acknowledgement exceeds %20"
-  alarm_actions             = [data.aws_sns_topic.alarm_notifications.arn]
-
-  metric_query {
-    id          = "e1"
-    expression  = "(acknowledgement+0.1)/(received+0.1)*100"
-    label       = "Expression"
-    return_data = "true"
-  }
-
-  metric_query {
-    id = "received"
-
-    metric {
-      metric_name = "NumberOfMessagesReceived"
-      namespace   = local.sqs_namespace
-      period      = "300"
-      stat        = "Sum"
-      unit        = "Count"
-
-      dimensions = {
-        QueueName = aws_sqs_queue.incoming_nems_events.name
-      }
-    }
-  }
-
-  metric_query {
-    id = "acknowledgement"
-
-    metric {
-      metric_name = "NumberOfMessagesDeleted"
-      namespace   = local.sqs_namespace
-      period      = "300"
-      stat        = "Sum"
-      unit        = "Count"
-
-      dimensions = {
-        QueueName = aws_sqs_queue.incoming_nems_events.name
-      }
-    }
-  }
-}
-
 resource "aws_cloudwatch_metric_alarm" "nems_incoming_dlq" {
   alarm_name                = "${var.environment}-${var.component_name}-dlq-size"
   comparison_operator       = "GreaterThanThreshold"
